5dc5e3df8f28859858b45cc49f55d908a0fdf47f
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-07-28 03:46:16 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector(`#ingredients`);
+
+ingredients.map((el) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = el;
+  list.append(li);
+
+});
+
+console.log(list);
