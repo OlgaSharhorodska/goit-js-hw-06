2ece71e8ddfccbe5909bbd4804839be0b27c7d37
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-07-29 02:10:42 +0300

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,4 @@
-const validationInput = document.getElementById("validation-input")
+const validationInput = document.querySelector("#validation-input")
 
 // console.log(validationInput);
 
@@ -7,12 +7,13 @@ validationInput.addEventListener("blur", onBlur)
 function onBlur(evt) {
     const inputBlur = evt.currentTarget;
     
-    if (inputBlur.value.trim().lenght === Number(inputBlur.dataset.lenght)){
-        validationInput.classList.remove("invalid")
+   if (inputBlur.value.trim().length === Number(validationInput.dataset.length)) {
         validationInput.classList.add("valid");
+        validationInput.classList.remove("invalid")
     } else {
-        validationInput.classList.remove("valid")
         validationInput.classList.add("invalid");
+        validationInput.classList.remove("valid")
+        
     }
     
 }
