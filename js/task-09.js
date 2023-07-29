e7f7cf86796e21b2e807e8af61faa2ca138c0eb4
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-07-29 03:51:06 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const button = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", onClick);
+
+function onClick(evt) {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
