b3093be889b31925c151e2d5f36d68037d46d93d
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-07-29 21:08:44 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.target.elements;
+
+    const formData = {
+        email: email.value,
+        password: password.value
+    }
+
+    if (email.value === "" || password.value === "") {
+        return console.log("Заповніть логін та пароль!");
+    }
+    console.log(`Ваш логін ${email.value}, ваш пароль ${password.value}`);
+    evt.currentTarget.reset();
+}
