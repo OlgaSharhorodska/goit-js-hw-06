a72940276184b3c88c3a6d2a80a130ea82fba6cb
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-01 20:11:12 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
+
+const btnCreate = document.querySelector('.js-create');
+const btnDestroy = document.querySelector('.js-destroy');
+const input = document.querySelector('.js-input');
+const boxes = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', handlerCreate);
+btnDestroy.addEventListener('click', handlerDestroy);
+
+let size = 20;
+
+function handlerCreate() {
+  const count = Number(input.value);
+  
+  const elements = [];
+
+  for (let i = 0; i < count; i += 1){
+    size += 10;
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.background = getRandomHexColor();
+
+    input.value = 0;
+    elements.push(div);
+  }
+  boxes.append(...elements);
+}
+
+function handlerDestroy() {
+  boxes.innerHTML = "";
+  size = 20;
+  input.value = 0;
+}
+
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
